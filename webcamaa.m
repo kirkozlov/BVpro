e25e5c26896734373e0dda87d023420243c1dfad
kirkozlov <kirill.kozlov.fhaachen@gmail.com>
2015-12-10 22:00:36 +0100

    Находит доску
    выводит ее на экран.
    с большым количеством фигур показывает неприличные вещи.

--- webcamaa.m
+++ webcamaa.m
@@ -1,7 +1,15 @@
 
   clear cam;
    cam=webcam('M');
-mypoins=
+mypoins=[15 15; 15 25; 15 35; 15 45; 15 55; 15 65; 15 75;
+         25 15; 25 25; 25 35; 25 45; 25 55; 25 65; 25 75; 
+         35 15; 35 25; 35 35; 35 45; 35 55; 35 65; 35 75; 
+         45 15; 45 25; 45 35; 45 45; 45 55; 45 65; 45 75; 
+         55 15; 55 25; 55 35; 55 45; 55 55; 55 65; 55 75; 
+         65 15; 65 25; 65 35; 65 45; 65 55; 65 65; 65 75; 
+         75 15; 75 25; 75 35; 75 45; 75 55; 75 65; 75 75; 
+         ]; 
+     mypoins=10*mypoins;
 %   img=imread(strcat('k',int2str(2025),'.jpg'));
 %   img = snapshot(cam);
 for i=5000:6000
@@ -27,14 +35,35 @@ for i=5000:6000
 %         % Shading Korrektur: Ergebnis ist ein gleichmaessig beleuchtetes Bild
 %         img3 = double(img)./double(img2);
       [imagePoints,boardSize] = detectCheckerboardPoints(img);
-      
-     imshow(img);
+     % imagePoints=sort(imagePoints,1);
+     
+     imagePoints=sortrows(imagePoints);
+     for ip=0:7:42
+     imagePoints([ip+1 ip+2 ip+3 ip+4 ip+5 ip+6 ip+7 ],:)=sortrows(imagePoints([ip+1 ip+2 ip+3 ip+4 ip+5 ip+6 ip+7 ],:),2);
+     end
+      tforms = estimateGeometricTransform(imagePoints, mypoins,'projective');
+       warpedImage = imwarp(img, tforms);
+[imagePointsw,boardSizew] = detectCheckerboardPoints(warpedImage);
+minx=min(imagePointsw(:,1))-100;
+miny=min(imagePointsw(:,2))-100;
+wi=max(imagePointsw(:,1))-min(imagePointsw(:,1))+200;
+hi=max(imagePointsw(:,2))-min(imagePointsw(:,2))+200;
+rect=[ minx,miny,wi,hi ];
+I2 = imcrop(warpedImage,rect) ;
+imshow(I2);
+
+
+    
+   
+  
+%     
+     %plot(imagePointsw(:,1), imagePointsw(:,2), 'ro');
      
 %      img=img2;
 %      imshow(img);
-    hold on; 
-    
-    plot(imagePoints(:,1), imagePoints(:,2), 'ro');
-    title(strcat(int2str(i)));
-  
+%     hold on; 
+%     
+%     plot(imagePoints(:,1), imagePoints(:,2), 'ro');
+%     title(strcat(int2str(i)));
+%   
 end
